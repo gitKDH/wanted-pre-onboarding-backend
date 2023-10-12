69c101e722efbe430c1d5459b62f8c9fb740a58a
gitKDH <reg0212@naver.com>
2023-10-12 18:58:55 +0900

    UserDTO 생성

---
+++ src/main/java/com/report/wantedpreonboardingbackend/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.report.wantedpreonboardingbackend.dto;
+
+public class UserDTO {
+    private String name;
+
+    public UserDTO(){
+    }
+
+    public UserDTO(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
