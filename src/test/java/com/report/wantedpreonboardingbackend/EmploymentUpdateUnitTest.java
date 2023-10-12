d7af35b5c4ee8c55aeaf245eadd4b24ab9a0e3c6
gitKDH <reg0212@naver.com>
2023-10-12 22:40:26 +0900

    채용 공고 수정 메소드 테스트 수정

--- src/test/java/com/report/wantedpreonboardingbackend/EmploymentUpdateUnitTest.java
+++ src/test/java/com/report/wantedpreonboardingbackend/EmploymentUpdateUnitTest.java
@@ -12,7 +12,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -32,23 +31,17 @@ public class EmploymentUpdateUnitTest {
 
     @Test
     public void testUpdateEmployment() {
-        // 샘플 EmploymentDTO 데이터 생성
         EmploymentDTO employmentDTO = new EmploymentDTO(1L, "Updated Position", 60000, "Updated Detail", "Updated Skill");
 
-        // 샘플 Employment 데이터 생성
         Employment existingEmployment = new Employment("Sample Position", 50000, "Sample Detail", "Sample Skill", null);
         existingEmployment.setId(1L);
 
-        // employmentService.findEmploymentById가 호출될 때, 샘플 Employment을 리턴하도록 설정
         Mockito.when(employmentService.findEmploymentById(1L)).thenReturn(existingEmployment);
 
-        // EmploymentController를 통해 채용 정보 업데이트
         ResponseEntity<Map<String, Object>> response = employmentController.updateEmployment(1L, employmentDTO);
 
-        // HTTP 응답 코드 확인 (예: 200 - OK)
         assertEquals(HttpStatus.OK, response.getStatusCode());
 
-        // 업데이트된 채용 정보 객체를 가져오고 필드 값 확인
         Map<String, Object> updatedEmploymentResponse = response.getBody();
         assertNotNull(updatedEmploymentResponse);
         assertEquals("Updated Position", updatedEmploymentResponse.get("position"));
